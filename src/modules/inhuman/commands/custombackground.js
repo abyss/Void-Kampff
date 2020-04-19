26cd86af98344fcb41316dbc8ef70db4828a44b9
Abyss <abyss@voidinput.com>
2020-04-19 00:33:17 -0400

    Custom Penalties and Background management

---
+++ src/modules/inhuman/commands/custombackground.js
@@ -0,0 +1,60 @@
+const { send } = require('../../../utils/chat');
+const bot = require('../../../bot');
+const { RichEmbed } = require('discord.js');
+const { userColor } = require('../../../utils/colors');
+
+exports.run = async (msg, args) => {
+    let customBackgrounds = await bot.db.get(msg.guild, 'inhuman.customBackgrounds');
+    if (!Array.isArray(customBackgrounds)) customBackgrounds = [];
+
+    if (!args.length) {
+        const embed = new RichEmbed()
+            .setTitle('Custom backgrounds for this server:')
+            .setColor(userColor(bot.client.user, msg.guild));
+
+        const output = [];
+        customBackgrounds.forEach((background, i) => output.push(`**${i+1}.** ${background}`));
+        embed.setDescription(output.join('\n'));
+
+        await send(msg.channel, embed);
+    } else {
+        if (args[0] === 'del') {
+            let index = parseInt(args[1], 10);
+            if (isNaN(index)) return false;
+            else index--;
+
+            const removed = customBackgrounds.splice(index, 1);
+
+            await bot.db.set(msg.guild, 'inhuman.customBackgrounds', customBackgrounds);
+            await send(msg.channel, `The following background has been removed: \`${removed}\``);
+        } else if (args[0] === 'add') {
+            const added = args.slice(1).join(' ');
+            customBackgrounds.push(added);
+
+            await bot.db.set(msg.guild, 'inhuman.customBackgrounds', customBackgrounds);
+            await send(msg.channel, `The following background was added: \`${added}\``);
+        } else {
+            await send(msg.channel, 'If you want to add a custom background, use `add`, and if you want to delete a custom background, use `del`');
+            return false;
+        }
+
+    }
+    return true;
+};
+
+const usage = new Map();
+usage.set('', 'Lists all custom backgrounds');
+usage.set('add <background>', 'Adds the custom background <background> as written');
+usage.set('del <#>', 'Deletes the custom background at #');
+exports.usage = usage;
+
+exports.config = {
+    name: 'Custom background Management',
+    cmd: 'custombackground',
+    alias: ['custombg', 'custombgs', 'custombackgrounds', 'custombackgrounds'],
+    botPermissions: [], // Permissions needed by the bot to use this command.
+    defaultPermissions: ['MANAGE_GUILD'], // Default permissions to use this command by user
+    location: 'GUILD_ONLY', // 'GUILD_ONLY', 'DM_ONLY', 'ALL'
+    description: 'Control custom backgrounds',
+    debug: false
+};
---
+++ src/modules/inhuman/commands/custompenalty.js
@@ -0,0 +1,60 @@
+const { send } = require('../../../utils/chat');
+const bot = require('../../../bot');
+const { RichEmbed } = require('discord.js');
+const { userColor } = require('../../../utils/colors');
+
+exports.run = async (msg, args) => {
+    let customPenalties = await bot.db.get(msg.guild, 'inhuman.customPenalties');
+    if (!Array.isArray(customPenalties)) customPenalties = [];
+
+    if (!args.length) {
+        const embed = new RichEmbed()
+            .setTitle('Custom penalties for this server:')
+            .setColor(userColor(bot.client.user, msg.guild));
+
+        const output = [];
+        customPenalties.forEach((penalty, i) => output.push(`**${i+1}.** ${penalty}`));
+        embed.setDescription(output.join('\n'));
+
+        await send(msg.channel, embed);
+    } else {
+        if (args[0] === 'del') {
+            let index = parseInt(args[1], 10);
+            if (isNaN(index)) return false;
+            else index--;
+
+            const removed = customPenalties.splice(index, 1);
+
+            await bot.db.set(msg.guild, 'inhuman.customPenalties', customPenalties);
+            await send(msg.channel, `The following penalty has been removed: \`${removed}\``);
+        } else if (args[0] === 'add') {
+            const added = args.slice(1).join(' ');
+            customPenalties.push(added);
+
+            await bot.db.set(msg.guild, 'inhuman.customPenalties', customPenalties);
+            await send(msg.channel, `The following penalty was added: \`${added}\``);
+        } else {
+            await send(msg.channel, 'If you want to add a custom penalty, use `add`, and if you want to delete a custom penalty, use `del`');
+            return false;
+        }
+
+    }
+    return true;
+};
+
+const usage = new Map();
+usage.set('', 'Lists all custom penalties');
+usage.set('add <penalty>', 'Adds the custom penalty <penalty> as written');
+usage.set('del <#>', 'Deletes the custom penalty at #');
+exports.usage = usage;
+
+exports.config = {
+    name: 'Custom Penalty Management',
+    cmd: 'custompenalty',
+    alias: ['custompen', 'custompenalties', 'custompenaltys'],
+    botPermissions: [], // Permissions needed by the bot to use this command.
+    defaultPermissions: ['MANAGE_GUILD'], // Default permissions to use this command by user
+    location: 'GUILD_ONLY', // 'GUILD_ONLY', 'DM_ONLY', 'ALL'
+    description: 'Control custom penalties',
+    debug: false
+};
